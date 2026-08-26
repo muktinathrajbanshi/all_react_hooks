bdd9fcec907aa0cd365bd46a823c092781915d9c
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-26 21:31:47 +0545

    wow useState hook is completed

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 const App = () => {
+  // const [counter, setCounter] = useState(10);
+  // console.log(counter);
+
+  const [color, setColor] = useState("Red");
+
+  // let color = "Red";
+  // const changeColor = () => {
+  //   color = "Blue";
+  //   console.log(color);
+  // };
+
+  const changeColor = () => {
+    setColor("Blue");
+  };
+
   return (
     <>
       <div className="container">
-        <h1>My favourite color is is Red!</h1>
-        <button>Blue</button>
+        <h1>My favourite color is is {color}!</h1>
+        <button onClick={changeColor}>Blue</button>
       </div>
     </>
   );
