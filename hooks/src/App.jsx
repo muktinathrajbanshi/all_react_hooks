3166a4a69a1bcff2510bce0139fd2471bd740be4
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-27 22:31:47 +0545

    la yar aja dhilo vayo

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,22 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const increaseCount = () => {
-    setCount((prev) => prev + 1);
-    setCount((prev) => prev + 1);
-    setCount((prev) => prev + 1);
-    setCount((prev) => prev + 1);
-  };
-
   return (
     <>
-      <div className="container">
-        <h1>Count: {count}</h1>
-        <button onClick={increaseCount}>Increase by 4</button>
-      </div>
+      <div className="container">I've rendered {count} times!</div>
     </>
   );
 };
--- hooks/src/dummyUseState.jsx
+++ hooks/src/dummyUseState.jsx
@@ -28,3 +28,66 @@
 // };
 
 // export default App;
+
+// 2.
+// import { useState } from "react";
+// import "./App.css";
+
+// const App = () => {
+//   const [car, setCar] = useState({
+//     brand: "Ferrari",
+//     model: "Roma",
+//     year: "2026",
+//     color: "red",
+//   });
+
+//   const changeColor = () => {
+//     setCar((prev) => {
+//       return { ...prev, color: "blue" };
+//     });
+//   };
+
+//   // const [model, setModel] = useState("Roma");
+//   // const [year, setYear] = useState("2026");
+//   // const [color, setColor] = useState("red");
+
+//   return (
+//     <>
+//       <div className="container">
+//         <h1>My {car.brand}</h1>
+//         <h2>
+//           It us a {car.color} {car.model} from {car.year}
+//         </h2>
+//         <button onClick={changeColor}>Blue</button>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
+
+// 3:
+// import { useState } from "react";
+// import "./App.css";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   const increaseCount = () => {
+//     setCount((prev) => prev + 1);
+//     setCount((prev) => prev + 1);
+//     setCount((prev) => prev + 1);
+//     setCount((prev) => prev + 1);
+//   };
+
+//   return (
+//     <>
+//       <div className="container">
+//         <h1>Count: {count}</h1>
+//         <button onClick={increaseCount}>Increase by 4</button>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
