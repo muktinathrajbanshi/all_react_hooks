d557614bee3595caa37e82494b154676ab28b7f4
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-27 21:07:10 +0545

    another example for useState

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -2,31 +2,20 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [car, setCar] = useState({
-    brand: "Ferrari",
-    model: "Roma",
-    year: "2026",
-    color: "red",
-  });
+  const [count, setCount] = useState(0);
 
-  const changeColor = () => {
-    setCar((prev) => {
-      return { ...prev, color: "blue" };
-    });
+  const increaseCount = () => {
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
   };
-
-  // const [model, setModel] = useState("Roma");
-  // const [year, setYear] = useState("2026");
-  // const [color, setColor] = useState("red");
 
   return (
     <>
       <div className="container">
-        <h1>My {car.brand}</h1>
-        <h2>
-          It us a {car.color} {car.model} from {car.year}
-        </h2>
-        <button onClick={changeColor}>Blue</button>
+        <h1>Count: {count}</h1>
+        <button onClick={increaseCount}>Increase by 4</button>
       </div>
     </>
   );
