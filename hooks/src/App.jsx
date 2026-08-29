b1ef8dc1b4a3449f9e8ac2ba2c29c761034bfa86
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-29 22:19:25 +0545

    wow finally useRef hook is also completed

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -2,32 +2,18 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [value, setValue] = useState(0);
-  const count = useRef(0);
+  const inputElem = useRef();
 
-  useEffect(() => {
-    count.current = count.current + 1;
-  });
+  const btnClicked = () => {
+    console.log(inputElem.current);
+    inputElem.current.style.background = "blue";
+  };
 
   return (
     <>
       <div className="container">
-        <button
-          onClick={() => {
-            setValue((prev) => prev - 1);
-          }}
-        >
-          -1
-        </button>
-        <h1>{value}</h1>
-        <button
-          onClick={() => {
-            setValue((prev) => prev + 1);
-          }}
-        >
-          +1
-        </button>
-        <h1>Render Count: {count.current}</h1>
+        <input type="text" ref={inputElem} />
+        <button onClick={btnClicked}>Click Here</button>
       </div>
     </>
   );
