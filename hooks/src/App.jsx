580abbda6a1f515375fb69a17e534acf95a7813c
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-31 21:57:21 +0545

    Now it's over

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -3,17 +3,9 @@ import "./App.css";
 import Header from "./components/Header";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  const newFn = useCallback(() => {}, []);
-
   return (
     <>
-      <div className="container">
-        <Header newFn={newFn} />
-        <h1>{count}</h1>
-        <button onClick={() => setCount((prev) => prev + 1)}>Click Here</button>
-      </div>
+      <div className="container"></div>
     </>
   );
 };
---
+++ hooks/src/dummyUseCallback.jsx
@@ -0,0 +1,21 @@
+// import { useCallback, useMemo, useState } from "react";
+// import "./App.css";
+// import Header from "./components/Header";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   const newFn = useCallback(() => {}, []);
+
+//   return (
+//     <>
+//       <div className="container">
+//         <Header newFn={newFn} />
+//         <h1>{count}</h1>
+//         <button onClick={() => setCount((prev) => prev + 1)}>Click Here</button>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
