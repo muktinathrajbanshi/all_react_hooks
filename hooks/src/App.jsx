00a5da8b19ea496c1543d4e5012f4d8bb64d33de
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-31 11:03:12 +0545

    now let's focus on useCallBack hook

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -2,35 +2,13 @@ import { useMemo, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [number, setNumber] = useState(0);
-  const [counter, setCounter] = useState(0);
-
-  function cubeNum(num) {
-    console.log("Calculation done!");
-    return Math.pow(num, 3);
-  }
-
-  const result = useMemo(() => cubeNum(number), [number]);
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div className="container">
-        <input
-          type="number"
-          value={number}
-          onChange={(e) => {
-            setNumber(e.target.value);
-          }}
-        />
-        <h1>Cube of the number: {result}</h1>
-        <button
-          onClick={() => {
-            setCounter(counter + 1);
-          }}
-        >
-          Counter++
-        </button>
-        <h1>Counter: {counter}</h1>
+        <h1>{count}</h1>
+        <button onClick={() => setCount((prev) => prev + 1)}>Click Here</button>
       </div>
     </>
   );
