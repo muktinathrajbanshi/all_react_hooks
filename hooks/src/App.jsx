bab949c852dc2ebabc06dda2c1a728d817448ea5
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-01 11:31:10 +0545

    Now let's focus on useReducer hook buddy

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "./App.css";
-import Footer from "./components/Footer";
-import Profile from "./components/Profile";
+import { useReducer } from "react";
 
 const App = () => {
+  const initialState = { count: 0 };
+
+  const reducer = (state, action) => {
+    return { count: state.count + 1 };
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
       <div className="container">
-        <Profile />
-        <Footer />
+        <h1>{state.count}</h1>
+        <div className="btn">
+          <button onClick={() => setCount((prev) => prev + 1)}>Increase</button>
+          <button onClick={() => setCount((prev) => prev - 1)}>Decrease</button>
+        </div>
       </div>
     </>
   );
