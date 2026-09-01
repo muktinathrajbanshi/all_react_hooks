ac42c8b774523ce32c3d31cc3ed205e372a4155e
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-01 09:46:18 +0545

    wah bade vai kiya baat hain

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
     <>
-      <div className="container"></div>
+      <div className="container">
+        <Profile />
+        <Footer />
+      </div>
     </>
   );
 };
--- hooks/src/main.jsx
+++ hooks/src/main.jsx
@@ -1,5 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import ContextProvider from "./context/AppContext.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+);
