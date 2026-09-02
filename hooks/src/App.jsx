846ac66cbef20c3fa3da66f47cd07ddc2b743f25
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-02 15:25:19 +0545

    wow finally useLayoutEffect hook is also completed

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -14,6 +14,11 @@ const App = () => {
     <>
       <div className="container">
         <h2>Test Message</h2>
+        {Array(40000)
+          .fill("")
+          .map((item, index) => (
+            <li key={index}>{Math.pow(Math.random(), 10)}</li>
+          ))}
       </div>
     </>
   );
