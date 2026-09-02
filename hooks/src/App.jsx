fedc3fcd49e803affdc063cadb57b60845ba8e3b
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-02 18:29:13 +0545

    finally completed custom hook

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,14 +1,9 @@
-import { useEffect, useLayoutEffect, useState } from "react";
 import "./App.css";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 const App = () => {
-  const [name, setName] = useState(
-    localStorage.getItem("username") ? localStorage.getItem("username") : "",
-  );
-
-  useEffect(() => {
-    localStorage.setItem("username", name);
-  }, [name]);
+  const [name, setName] = useLocalStorage("username", "");
+  const [id, setId] = useLocalStorage("userid", "");
 
   return (
     <>
@@ -20,6 +15,14 @@ const App = () => {
           onChange={(e) => setName(e.target.value)}
         />
         <h2>Hello, {name}!</h2>
+
+        <input
+          type="text"
+          placeholder="Enter you id"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <h2>Your Id: {id}!</h2>
       </div>
     </>
   );
