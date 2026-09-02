1ebd1317862c4e05d93e599636ff8c13c94c20b8
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-02 18:30:10 +0545

    finally samapath

---
+++ hooks/src/customHook.jsx
@@ -0,0 +1,31 @@
+// import "./App.css";
+// import useLocalStorage from "./hooks/useLocalStorage";
+
+// const App = () => {
+//   const [name, setName] = useLocalStorage("username", "");
+//   const [id, setId] = useLocalStorage("userid", "");
+
+//   return (
+//     <>
+//       <div className="container">
+//         <input
+//           type="text"
+//           placeholder="Enter you name"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//         />
+//         <h2>Hello, {name}!</h2>
+
+//         <input
+//           type="text"
+//           placeholder="Enter you id"
+//           value={id}
+//           onChange={(e) => setId(e.target.value)}
+//         />
+//         <h2>Your Id: {id}!</h2>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
